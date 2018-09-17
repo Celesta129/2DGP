8ff7a62da6d2f9f5d270c951f6cfaf7e76d61d62
김기태 <celesta129@naver.com>
2018-09-17 19:41:04 +0900

    Commit 02
    동작 구조 수정.

--- Drills/Drill-05/Drill-05.py
+++ Drills/Drill-05/Drill-05.py
@@ -3,16 +3,25 @@ open_canvas()
 character = load_image("animation_sheet.png")
 grass = load_image("grass.png")
 
-position = [(203, 535), (132, 243), (535, 470), (477, 203), (715, 136),
+Arr_position = [(203, 535), (132, 243), (535, 470), (477, 203), (715, 136),
             (316, 225), (510, 92), (692, 518), (682, 336), (712, 349)]
+
+frame = 0
+posNumber = 0
+position = Arr_position[posNumber]
 
 
 def move_character():
     pass
 
 
+def draw():
+    pass
+
+
 while True:
     move_character()
+    draw()
     pass
 
 close_canvas()
