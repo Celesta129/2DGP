8be6f15f085a6e68388516c7bc0e2aea0e0e82ba
김기태 <celesta129@naver.com>
2018-09-17 20:26:42 +0900

    Commit 07
    진행에 따라 점점 계산시 오차때문에 갈아엎음

--- Drills/Drill-05/Drill-05.py
+++ Drills/Drill-05/Drill-05.py
@@ -14,53 +14,9 @@ y = Arr_position[0][1]
 
 
 def move_character():
-    global x
-    global y
-    global PosIndex
-
-    nextIndex = PosIndex["Next"]
-    currentIndex = PosIndex["Current"]
-
-    x = x + (Arr_position[nextIndex][0] - Arr_position[currentIndex][0]) * 0.05
-    y = y + (Arr_position[nextIndex][1] - Arr_position[currentIndex][1]) * 0.05
-
-    xCheck = Arr_position[nextIndex][0]*0.95 < x <= Arr_position[nextIndex][0] * 1.05
-    yCheck = Arr_position[nextIndex][1]*0.95 < y <= Arr_position[nextIndex][1] * 1.05
-
-    Change_Direction()
-
-    if xCheck and yCheck:
-        PosIndex["Next"] = PosIndex["Next"] + 1
-        PosIndex["Current"] = PosIndex["Current"] +1
-        if PosIndex["Next"] == 10:
-            PosIndex["Next"] = 0
-
-        if PosIndex["Current"] == 10:
-            PosIndex["Current"] = 0
-        pass
-
     pass
-
 
 def draw():
-    global frame
-    global x
-    global y
-
-    clear_canvas()
-    grass.draw(400, 30)
-
-    if direction == "Left":
-        character.clip_draw(frame * 100, 0, 100, 100, x, y)
-    else:
-        character.clip_draw(frame * 100, 100, 100, 100, x, y)
-
-    update_canvas()
-    frame = (frame + 1) % 8
-
-    pass
-
-def Change_Direction():
     pass
 
 while True:
