d25afa0521a6fc106a973a3cf376068a2299dff4
김기태 <celesta129@naver.com>
2018-09-17 19:52:28 +0900

    Commit 03
    Draw와 좌표조작을 위한 변수들과 Draw 완성

--- Drills/Drill-05/Drill-05.py
+++ Drills/Drill-05/Drill-05.py
@@ -8,7 +8,9 @@ Arr_position = [(203, 535), (132, 243), (535, 470), (477, 203), (715, 136),
 
 frame = 0
 posNumber = 0
-position = Arr_position[posNumber]
+direction = "Right"
+x = Arr_position[posNumber][0]
+y = Arr_position[posNumber][1]
 
 
 def move_character():
@@ -16,12 +18,31 @@ def move_character():
 
 
 def draw():
+    global frame
+    global x
+    global y
+    global posNumber
+
+    clear_canvas()
+    grass.draw(400, 30)
+
+    if direction == "Left":
+        character.clip_draw(frame * 100, 0, 100, 100, x, y)
+    else:
+        character.clip_draw(frame * 100, 100, 100, 100, x, y)
+
+    update_canvas()
+    frame = (frame + 1) % 8
+
     pass
 
 
 while True:
     move_character()
     draw()
+
+    delay(0.05)
+    get_events()
     pass
 
 close_canvas()
