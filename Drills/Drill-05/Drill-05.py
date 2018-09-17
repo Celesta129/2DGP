9dc534ee94407e05cb0d35f396932448636b28e5
Celesta129(C221) <celesta129@naver.com>
2018-09-17 15:11:28 +0900

    2
    positiom tuple list 추가
    run_charactor 구성

--- Drills/Drill-05/Drill-05.py
+++ Drills/Drill-05/Drill-05.py
@@ -1,8 +1,50 @@
 from pico2d import *
 
-position = ({203,535},{213,243},{535,470},{477,203},{715,136},{316,225},{510,92},{692,518},{682,336},{712,349})
-
 open_canvas()
-while(True):
+character = load_image('animation_sheet.png')
+
+position = [(203, 535), (213, 243), (535, 470), (477, 203), (715, 136), (316, 225), (510, 92), (692, 518), (682, 336), (712, 349)]
+x = position[0]
+frame = 0
+
+def run_charactor():
+    run_pos1()
+    run_pos2()
+    run_pos3()
+    run_pos4()
+    run_pos5()
+    run_pos6()
+    run_pos7()
+    run_pos8()
+    run_pos9()
+    run_pos10()
     pass
+
+def run_pos1():
+    pass
+def run_pos2():
+    pass
+def run_pos3():
+    pass
+def run_pos4():
+    pass
+def run_pos5():
+    pass
+def run_pos6():
+    pass
+def run_pos7():
+    pass
+def run_pos8():
+    pass
+def run_pos9():
+    pass
+def run_pos10():
+    pass
+
+
+while(True):
+    clear_canvas()
+    run_charactor()
+    character.clip_draw(frame * 100, 0, 100, 100, x, 90)
+    update_canvas()
 close_canvas()
