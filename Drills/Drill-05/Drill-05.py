915fdeab33a10321a546e53b86d12d158ea4f70e
Celesta129(C221) <celesta129@naver.com>
2018-09-17 15:25:05 +0900

    3
    pos1 기본로직 구성

--- Drills/Drill-05/Drill-05.py
+++ Drills/Drill-05/Drill-05.py
@@ -4,7 +4,6 @@ open_canvas()
 character = load_image('animation_sheet.png')
 
 position = [(203, 535), (213, 243), (535, 470), (477, 203), (715, 136), (316, 225), (510, 92), (692, 518), (682, 336), (712, 349)]
-x = position[0]
 frame = 0
 
 def run_charactor():
@@ -20,8 +19,20 @@ def run_charactor():
     run_pos10()
     pass
 
+
 def run_pos1():
+    x = position[0][0]
+    y = position[0][1]
+    while x != position[1][0] and y != position[1][1]:
+        clear_canvas()
+        x = x + (position[1][0] - position[0][0]) * 0.1
+        y = y + (position[1][1] - position[0][1]) * 0.01
+        character.clip_draw(frame * 100, 0, 100, 100, x, y)
+        update_canvas()
+        delay(0.1)
     pass
+
+
 def run_pos2():
     pass
 def run_pos3():
@@ -43,8 +54,7 @@ def run_pos10():
 
 
 while(True):
-    clear_canvas()
+
     run_charactor()
-    character.clip_draw(frame * 100, 0, 100, 100, x, 90)
-    update_canvas()
+
 close_canvas()
