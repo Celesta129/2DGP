a3bf4b49f28d952b062cc0a150a08231f9c6e907
Celesta129(C221) <celesta129@naver.com>
2018-09-17 14:59:17 +0900

    Create While
    position 값 세팅,
    run_charactor

--- Drills/Drill-05/Drill-05.py
+++ Drills/Drill-05/Drill-05.py
@@ -1,9 +1,8 @@
 from pico2d import *
 
-def move_charactor():
-    pass
+position = ({203,535},{213,243},{535,470},{477,203},{715,136},{316,225},{510,92},{692,518},{682,336},{712,349})
+
 open_canvas()
-while(True)
-
-
+while(True):
+    pass
 close_canvas()
