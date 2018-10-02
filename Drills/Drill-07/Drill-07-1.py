60304218d9de1b40aa436c1a4a59ebe061995f02
김기태 <celesta129@naver.com>
2018-10-02 16:56:35 +0900

    기본 출력 로직 완성
    다음은 이동로직

--- Drills/Drill-07/Drill-07-1.py
+++ Drills/Drill-07/Drill-07-1.py
@@ -1,20 +1,46 @@
 from pico2d import *
 
 import random
+
+
+
+
 open_canvas()
 
 character = load_image('animation_sheet.png')
-frame = 0
 ground = load_image('KPU_GROUND.png')
 
-points = [(random.randint(-500,500),random.randint(-300,300)) for n in range(20)]
+
+frame = 0
+param = 0
+
+points = [(random.randint(-350,350),random.randint(-250,250)) for n in range(20)]
+
+characterX = points[0][0]
+characterY = points[0][1]
+
 
 def draw():
+    global characterX, characterY
+    clear_canvas()
+    ground.draw(400,300)
+    character.clip_draw(frame * 100,0,100,100, characterX, characterY)
+    update_canvas()
+    delay(0.01)
+    get_events()
     pass
-def move_charactor():
+
+
+def move_character():
+    global param
+
+
+    param += 0.01
     pass
+
+
 while True:
-    move_charactor()
+    move_character()
     draw()
 
 close_canvas()
