0b8fd63e1a0235a85e6b07c2ad6b4a6703a57830
김기태 <celesta129@naver.com>
2018-10-02 16:42:55 +0900

    기초로직 구성
    while True: movecharactor, draw

--- Drills/Drill-07/Drill-07-1.py
+++ Drills/Drill-07/Drill-07-1.py
@@ -1,11 +1,20 @@
 from pico2d import *
 
 import random
+open_canvas()
 
 character = load_image('animation_sheet.png')
+frame = 0
 ground = load_image('KPU_GROUND.png')
 
 points = [(random.randint(-500,500),random.randint(-300,300)) for n in range(20)]
-open_canvas()
+
+def draw():
+    pass
+def move_charactor():
+    pass
+while True:
+    move_charactor()
+    draw()
 
 close_canvas()
