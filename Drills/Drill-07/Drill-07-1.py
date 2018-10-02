fdd6a781cf7e426f0d528ce725cb2d0db1362cb4
김기태 <celesta129@naver.com>
2018-10-02 16:40:20 +0900

    points 생성
    20개 랜덤생성

--- Drills/Drill-07/Drill-07-1.py
+++ Drills/Drill-07/Drill-07-1.py
@@ -2,5 +2,10 @@ from pico2d import *
 
 import random
 
+character = load_image('animation_sheet.png')
+ground = load_image('KPU_GROUND.png')
+
+points = [(random.randint(-500,500),random.randint(-300,300)) for n in range(20)]
 open_canvas()
+
 close_canvas()
