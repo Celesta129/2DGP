6e807d0e4bf6646684ab2b8fd1c504081c7acc94
김기태 <celesta129@naver.com>
2018-11-20 16:32:46 +0900

    Drill14
    공 출력문제 해결중

---
+++ Drills/Drill-14/ball.py
@@ -0,0 +1,26 @@
+import random
+from pico2d import *
+import game_world
+import game_framework
+
+class Ball:
+    image = None
+
+    def __init__(self):
+        if Ball.image == None:
+            Ball.image = load_image('ball21x21.png')
+        self.x, self.y, self.fall_speed = random.randint(0, 1600-1), random.randint(0, 1600-1), 0
+
+    def set_object(self,object):
+        pass
+    def get_bb(self):
+        return self.x - 10, self.y - 10, self.x + 10, self.y + 10
+
+    def draw(self):
+        self.image.draw(self.x, self.y)
+        draw_rectangle(*self.get_bb())
+
+    def update(self):
+        pass
+
+
